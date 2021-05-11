493a443ac05a2d988080ee3342f6e193e94c6aa0
Joel Kaye <kayej22@hotmail.com>
2021-05-11 09:40:21 -0600

    updating delete function

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -37,12 +37,24 @@ class TodoContainer extends React.Component {
     }));    
   };
 
+  delTodo = id => {
+    this.setState({
+      todos: [
+        ...this.state.todos.filter(todo => {
+          return todo.id !== id;
+        })
+      ]
+    })
+  }
 
   render () {
     return (
       <div>
         <Header />
-        <TodosList todos={this.state.todos} />
+        <TodosList
+        todos={this.state.todos}
+        deleteTodoProps={this.delTodo}
+        />
       </div>
     );
   }
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -3,10 +3,15 @@ import React from "react"
 
 
 class TodoItem extends React.Component {
+
   render() {
     return (
     <li>
+      <button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>
+        Delete
+        </button>
       <input
+    
       type="checkbox"
       checked={this.props.todo.completed}
       onChange={() => this.props.handleChangeProps(this.props.todo.id)}/>
