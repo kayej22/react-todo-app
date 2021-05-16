5a62c6bea601c9619969989a19e8d9b28b9d3f0a
Joel Kaye <kayej22@hotmail.com>
2021-05-16 11:31:58 -0600

    working on edit feild

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,6 +4,17 @@ import styles from "./TodoItem.module.css"
 
 class TodoItem extends React.Component {
 
+  handleEditing = () => {
+    this.setState({
+      editing: true,
+    })
+  }
+
+
+  state = {
+    editing: false,
+  }
+
   render() {
 
     const completedStyle = {
@@ -15,8 +26,18 @@ class TodoItem extends React.Component {
 
     const { completed, id, title } = this.props.todo
 
+    let viewMode = {}
+    let editMode = {}
+
+    if (this.state.editing) {
+      viewMode.display  = "none"
+    } else {
+      editMode.display = "none"
+    }
+
     return (
     <li className={styles.item}>
+      <div onDoubleClick={this.handleEditing} style={viewMode}>
       <input
       type="checkbox"
       className={styles.checkbox}
@@ -25,6 +46,8 @@ class TodoItem extends React.Component {
     />
       <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
       <span style={completed ? completedStyle : null}>{title}</span>
+      </div>
+      <input type="text" style={editMode} className={styles.textInput} />
     </li>
     )
   }
