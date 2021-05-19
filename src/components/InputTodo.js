8bf0478501ecf8a605189872a13837dfcf47394d
Joel Kaye <kayej22@hotmail.com>
2021-05-19 09:04:51 -0600

    cleaning up inputtodo hooks

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -3,8 +3,7 @@ import React, { useState } from "react"
 
   const InputTodo = props => {
     const [inputText, setInputText] = useState({
-      fName:"",
-      lastName:"",
+      title: "",
     })
   
 
@@ -17,35 +16,30 @@ import React, { useState } from "react"
 
   const handleSubmit = e => {
     e.preventDefault()
-    console.log("submitted")
+    if(inputText.title.trim()) {
+      props.addTodoProps(inputText.title)
+      setInputText({
+        title: "",
+      })
+    } else {
+      alert("Please write item")
+      }
     }
   
 
     return (
-      <>
         <form onSubmit= {handleSubmit} className="form-container">
           <input
             type= "text"
             className="input-text"
-            placeholder= "Add first name"
-            value={inputText.fName}
-            name="fName"
+            placeholder= "Add todo..."
+            value={inputText.title}
+            name="title"
             onChange={onChange}
-          />
-          <input
-            type="text"
-            className="input-text"
-            placeholder="Add last name"
-            value={inputText.lastName}
-            name="lastName"
-            onChange={onChange}
-          />
-
+        />
         <button className="input-submit">Submit</button>
       </form>
-      <h2>{inputText.fName}</h2>
-      <h2>{inputText.lastName}</h2>
-    </>
+      
   )
 }
 export default InputTodo
